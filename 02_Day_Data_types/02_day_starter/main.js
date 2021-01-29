943688a2876f733f9ce2d338b1d829e85d1de4f9
Yahel Rosen <thederociteamhd@gmail.com>
2021-01-29 13:13:05 +0200

    finished day 2 with level 3

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -43,4 +43,15 @@ console.log(weird.substr(weird.indexOf('because'), 'because '.length * 3))
 // Level 3
 let love_sen = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
 console.log(love_sen.toLowerCase().split('love').length - 1)
-console.log((weird.match('/because/g') || []).length)
+console.log(weird.match(new RegExp('because', 'g')).length)
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+console.log(sentence.replace(/[^\w ]/g, ''))
+let income_sen = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+let num_only = (val) => {
+    if (!isNaN(val)) {
+        return val;
+    }
+}
+income_sen = income_sen.split(' ').filter(num_only)
+income_sen = income_sen.map((val) => parseInt(val))
+console.log(income_sen.reduce((a, b) => a + b, 0))
